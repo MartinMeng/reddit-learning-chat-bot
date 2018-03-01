bcda20689306229d3b7928a18e917aee99ec22cb
MartinMeng <haoxingxiaohui@gmail.com>
2018-03-01 15:49:11 +0800

    post submissions from 4 years ago instead of 1
    also, rm settings and add settings_sample

--- reddit.py
+++ reddit.py
@@ -4,25 +4,24 @@ import random
 import bot
 import settings
 
-api = praw.Reddit(client_id=settings.REDDIT_CLIENT_ID,
-                     client_secret=settings.REDDIT_SECRET,
-                     password=settings.REDDIT_PASSWORD,
-                     user_agent=settings.REDDIT_USER_AGENT,
-                     username=settings.REDDIT_USERNAME)
-
+seedAccount = praw.Reddit(client_id=settings.seedAccount['REDDIT_CLIENT_ID'],
+                     client_secret=settings.seedAccount['REDDIT_SECRET'],
+                     password=settings.seedAccount['REDDIT_PASSWORD'],
+                     user_agent=settings.seedAccount['REDDIT_USER_AGENT'],
+                     username=settings.seedAccount['REDDIT_USERNAME'])
 
 def submission_timespan():
-    # Get the current epoch time, and then subtract one year
-    year_ago = int(time.time()) - 31622400
-    # Add a day to the time from a year ago
-    end_search = year_ago + 86400
+    # Get the current epoch time, and then subtract four years
+    start_search = int(time.time()) - 126144000
+    # Add 1 year to the start time
+    end_search = start_search + 31536000
     # Return a tuple with the start/end times to search old submissions
-    return (year_ago, end_search)
+    return (start_search, end_search)
 
 
 def random_submission():
-    # Get submissions from a random subreddit one year ago
-    submissions = list(api.subreddit('random').submissions(
+    # Get submissions from a random subreddit four years ago
+    submissions = list(seedAccount.subreddit('random').submissions(
         *submission_timespan()))
 
     # Check if there's any items in the submissions list. If not display error
@@ -43,7 +42,7 @@ def random_submission():
                 params = {"title":chosen.title, "url":chosen.url}
 
             # Submit the same content to the same subreddit. Prepare your salt picks
-            api.subreddit(chosen.subreddit.display_name).submit(**params)
+            submission = seedAccount.subreddit(chosen.subreddit.display_name).submit(**params)
         except Exception as e:
             print e
 
@@ -53,7 +52,7 @@ def random_submission():
 
 def random_reply():
     # Choose a random submission from /r/all that is currently hot
-    submission = random.choice(list(api.subreddit('all').hot()))
+    submission = random.choice(list(seedAccount.subreddit('all').hot()))
     # Replace the "MoreReplies" with all of the submission replies
     submission.comments.replace_more(limit=0)
 
---
+++ settings_sample.py
@@ -0,0 +1,6 @@
+seedAccount = {}
+seedAccount['REDDIT_CLIENT_ID'] = 'REPLACE_ME'
+seedAccount['REDDIT_SECRET'] = 'REPLACE_ME'
+seedAccount['REDDIT_PASSWORD'] = 'REPLACE_ME'
+seedAccount['REDDIT_USERNAME'] = 'REPLACE_ME'
+seedAccount['REDDIT_USER_AGENT'] = 'REPLACE_ME'
